01d7bd877316e2647f6de79b40b04aeae533c11d
Renan Lopes <renan.lopes@nextpage.com.br>
2023-12-27 16:17:50 -0300

    database

--- A04/back/database/database.js
+++ A04/back/database/database.js
@@ -3,7 +3,7 @@ import mysql  from 'mysql2';
 const database = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'if_prog_sist_corp'
+    database: 'if_pam_usuarios'
   });
 
 export default database.promise()
